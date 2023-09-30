a74dc60e283dbfdaf365edfa48c85548d5d717e2
lomakina-mariya <lomakina.ms@gmail.com>
2023-09-30 17:32:13 +0300

    Commit after review 1 (without renaming buttons)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,34 +1,42 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Masha-MBP on 22.09.2023.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var plusUIButton: UIButton!
-    @IBOutlet weak var counterUILabel: UILabel!
-    @IBOutlet weak var minusUIButton: UIButton!
-    @IBOutlet weak var resetUIButton: UIButton!
-    @IBOutlet weak var histotyUITextView: UITextView!
+final class ViewController: UIViewController {
+    @IBOutlet weak private var plusUIButton: UIButton!
+    @IBOutlet weak private var counterUILabel: UILabel!
+    @IBOutlet weak private var minusUIButton: UIButton!
+    @IBOutlet weak private var resetUIButton: UIButton!
+    @IBOutlet weak private var historyUITextView: UITextView!
     private var sum: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         plusUIButton.layer.cornerRadius = 25
         minusUIButton.layer.cornerRadius = 25
         resetUIButton.layer.cornerRadius = 25
-        
     }
-    @IBAction func plusButtonDidTap(_ sender: Any) {
+    
+    private func updateCounter() {
+        counterUILabel.text = "Значение счетчика: \(sum)"
+    }
+    
+    private func updateHistory(change: String) {
+        historyUITextView.text.append("\n[\(getCurrentDate())] \(change)")
+        historyUITextView.scrollRangeToVisible(NSRange(location: -1, length: 1))
+    }
+    
+    private func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return dateFormatter.string(from: Date())
+    }
+    
+    @IBAction private func plusButtonDidTap(_ sender: Any) {
         sum += 1
         updateCounter()
         updateHistory(change: "Значение изменено на +1")
     }
     
-    @IBAction func minusButtonDidTap(_ sender: Any) {
+    @IBAction private func minusButtonDidTap(_ sender: Any) {
         if sum > 0 {
             sum -= 1
             updateCounter()
@@ -38,25 +46,12 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func resetButtonDidTap(_ sender: Any) {
+    @IBAction private func resetButtonDidTap(_ sender: Any) {
         sum = 0
         updateCounter()
         updateHistory(change: "Значение сброшено")
     }
     
-    func updateCounter() {
-        counterUILabel.text = "Значение счетчика: \(sum)"
-    }
     
-    func updateHistory(change: String) {
-        histotyUITextView.text.append("\n[\(getCurrentDate())] \(change)")
-        histotyUITextView.scrollRangeToVisible(NSRange(location: -1, length: 1))
-    }
-    
-    func getCurrentDate() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-        return dateFormatter.string(from: Date())
-    }
 }
 
